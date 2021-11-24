c70bc130f93a1b3014f2a891193a2a31ffd6e075
Áron Tóth <aron.toth2@stud.uni-corvinus.hu>
2021-11-24 18:30:24 +0100

    6. Hozzáadás funkció

--- UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/Form1.Designer.cs
@@ -28,13 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listUsers = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelFirstname = new System.Windows.Forms.Label();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxFirstname = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listUsers
+            // 
+            this.listUsers.FormattingEnabled = true;
+            this.listUsers.Location = new System.Drawing.Point(47, 36);
+            this.listUsers.Name = "listUsers";
+            this.listUsers.Size = new System.Drawing.Size(229, 238);
+            this.listUsers.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(338, 124);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(202, 47);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "button1";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(295, 39);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(35, 13);
+            this.labelLastName.TabIndex = 2;
+            this.labelLastName.Text = "label1";
+            // 
+            // labelFirstname
+            // 
+            this.labelFirstname.AutoSize = true;
+            this.labelFirstname.Location = new System.Drawing.Point(295, 73);
+            this.labelFirstname.Name = "labelFirstname";
+            this.labelFirstname.Size = new System.Drawing.Size(35, 13);
+            this.labelFirstname.TabIndex = 3;
+            this.labelFirstname.Text = "label2";
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(382, 39);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(158, 20);
+            this.textBoxLastName.TabIndex = 4;
+            // 
+            // textBoxFirstname
+            // 
+            this.textBoxFirstname.Location = new System.Drawing.Point(382, 73);
+            this.textBoxFirstname.Name = "textBoxFirstname";
+            this.textBoxFirstname.Size = new System.Drawing.Size(158, 20);
+            this.textBoxFirstname.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxFirstname);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.labelFirstname);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listUsers);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listUsers;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelFirstname;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxFirstname;
     }
 }
 
--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();  //User.cs ->namespace UserMaintenance.Entities
+
         public Form1()
         {
             InitializeComponent();
+            labelLastName.Text = Resource1.LastName;
+            labelFirstname.Text = Resource1.FirstName;
+            buttonAdd.Text = Resource1.Add;
+
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User
+            {
+                LastName = textBoxLastName.Text,
+                FirstName = textBoxFirstname.Text
+            };
+
+            users.Add(u);
+
         }
     }
 }
