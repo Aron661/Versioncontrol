cdc730063c96ac6698cc41655bd98d9909fa149c
Áron Tóth <aron.toth2@stud.uni-corvinus.hu>
2021-11-25 19:25:18 +0100

    Adatok betöltése

--- ExcelExportos/Form1.cs
+++ ExcelExportos/Form1.cs
@@ -12,11 +12,18 @@ namespace ExcelExportos
 {
     public partial class Form1 : Form
     {
-
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities(); //példányosítsd az ORM objektumot!
 
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        void LoadData() 
+        {
+            Flats = context.Flats.ToList();  //másold az adattáblát a memóriába! ToList() fv
         }
     }
 }
