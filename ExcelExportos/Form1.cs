f12a7f96cb9f9ded2703765802e0623ea4522481
Áron Tóth <aron.toth2@stud.uni-corvinus.hu>
2021-11-26 12:18:08 +0100

    Kimenet formázása

--- ExcelExportos/Form1.cs
+++ ExcelExportos/Form1.cs
@@ -21,6 +21,11 @@ namespace ExcelExportos
         Excel.Workbook xlWB; // A létrehozott munkafüzet
         Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
 
+        string[] headers = new string[] {"Kód","Eladó","Oldal","Kerület","Lift","Szobák száma",
+             "Alapterület (m2)",
+             "Ár (mFt)",
+             "Négyzetméter ár (Ft/m2)"};   //header tömb felvétele //kikellett szervezni az osztály szintjére a FormatTable() miatt
+
         public Form1()
         {
             InitializeComponent();
@@ -80,17 +85,61 @@ namespace ExcelExportos
                 values[counter, 1] = item.Vendor;
                 values[counter, 2] = item.Side;
                 values[counter, 3] = item.District;
-                values[counter, 4] = item.Elevator;
+                if (item.Elevator==true)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+                //values[counter, 4] = item.Elevator;
                 values[counter, 5] = item.NumberOfRooms;
                 values[counter, 6] = item.FloorArea;
                 values[counter, 7] = item.Price;
-                values[counter, 8] = "="+ GetCell(counter+2,8) + "*1000000/" + GetCell(counter + 2, 7); //számított érték képlettel counter +2 tömb és excel elcsúszás miatt --pl =H2 cellában == "="+ GetCell(counter+2,8))
+                values[counter, 8] = "="+ GetCell(counter+2,8) + "*1000000/" + GetCell(counter + 2, 7); //számított érték képlettel counter +2 tömb és excel elcsúszás miatt + fejléc --pl =H2 cellában == "="+ GetCell(counter+2,8))
                 counter++;
             }
 
             xlSheet.get_Range(
                               GetCell(2, 1),
                               GetCell(1 + values.GetLength(0),values.GetLength(1))).Value2=values;  //Range, írd ki a values tömb tartalmát az Excel fájlba.
+
+            FormatTable();
+        }
+
+        private void FormatTable()
+        {
+            Excel.Range headerRange = xlSheet.get_Range(
+                GetCell(1, 1), GetCell(1, headers.Length));   //a header range felvétele
+
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            int lastRowID =xlSheet.UsedRange.Rows.Count ;
+            Excel.Range tablarange = xlSheet.get_Range(
+                GetCell(2, 1), GetCell(lastRowID,headers.Length)); //a tábla,adatok range felvétele
+
+            tablarange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick); //Az egész táblának is legyen olyan körbe szegélye, mint a fejlécnek
+
+            Excel.Range elsooszlop = xlSheet.get_Range(
+                GetCell(2,1), GetCell(lastRowID,1)); //első oszlop adatok range felvétele
+
+            elsooszlop.Font.Bold = true;
+            elsooszlop.Interior.Color = Color.LightYellow;  //Az első oszlop adatai legyenek félkövérek és a háttér legyen halvány sárga
+
+            Excel.Range utolsooszlop = xlSheet.get_Range(
+                GetCell(2, headers.Length), GetCell(lastRowID, headers.Length)); //utolsó oszlop adatok range felvétele
+
+            utolsooszlop.Interior.Color = Color.LightGreen; //Az utolsó oszlop adatainak háttere legyen halványzöld
+
+            utolsooszlop.NumberFormat = "#,###.00_;";  //"$#,##0.00_);[Red]($#,##0.00)" -> pl: 227 272,73 Ft
+            //Az utolsó oszlop adatai két tizedesre kerekített formában jelenjenek meg. (Google)
         }
 
         void LoadData()
