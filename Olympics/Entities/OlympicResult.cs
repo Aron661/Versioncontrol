ad481a9def42c0bb8bb32374dc4d77558ed1f2af
Áron Tóth <aron.toth2@stud.uni-corvinus.hu>
2021-12-05 18:10:00 +0100

    Adatmodell + adatbetöltés

---
+++ Olympics/Entities/OlympicResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olympics.Entities
+{
+    public class OlympicResult
+    {
+        public int Year { get; set; }
+        public string Country { get; set; }
+        public int[] Medals { get; set; }  //int[] tömb
+        public int Position { get; set; }
+    }
+}
--- Olympics/Form1.cs
+++ Olympics/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Olympics.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,38 @@ namespace Olympics
 {
     public partial class Form1 : Form
     {
+        List<OlympicResult> results = new List<OlympicResult>();  //Hozz létre egy OlympicResult-okból álló listát a Form1 osztály szintjén results néven.
+
         public Form1()
         {
             InitializeComponent();
+            betoltes("Summer_olympic_Medals.csv");
+
+        }
+
+        private void betoltes(string fajlnev)
+        {
+            using (StreamReader sr= new StreamReader(fajlnev)) 
+            {
+                sr.ReadLine(); //első sor beolvasása csv.ből fejléc - amivel nem csinálunk "semmit"
+                while (!sr.EndOfStream) 
+                {
+                    string sor = sr.ReadLine(); //csv sora
+                    string[] mezok = sor.Split(',');
+
+                    OlympicResult peldany = new OlympicResult();
+                    peldany.Year = int.Parse(mezok[0]);
+                    peldany.Country = mezok[3];
+                    //5. 6. 7. oszlop arany , ezüst , bronz
+                    int[] mtomb = new int[3]; //3 elemű tömb felvétele
+                    mtomb[0] = int.Parse(mezok[5]); //arany
+                    mtomb[1] = int.Parse(mezok[6]); //ezüst
+                    mtomb[2] = int.Parse(mezok[7]); //bronz
+
+                    results.Add(peldany);
+
+                }
+            } 
         }
     }
 }
