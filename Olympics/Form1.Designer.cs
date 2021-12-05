cb5a73f46ccab1f45a8b28e4aaaf3f4468703a92
Áron Tóth <aron.toth2@stud.uni-corvinus.hu>
2021-12-05 20:16:08 +0100

    évvélaszt + sorrend + excel export

--- Olympics/Form1.Designer.cs
+++ Olympics/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label_year = new System.Windows.Forms.Label();
+            this.comboBox_evek = new System.Windows.Forms.ComboBox();
+            this.btn_excel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_year
+            // 
+            this.label_year.AutoSize = true;
+            this.label_year.Location = new System.Drawing.Point(78, 50);
+            this.label_year.Name = "label_year";
+            this.label_year.Size = new System.Drawing.Size(35, 13);
+            this.label_year.TabIndex = 0;
+            this.label_year.Text = "Évek:";
+            // 
+            // comboBox_evek
+            // 
+            this.comboBox_evek.FormattingEnabled = true;
+            this.comboBox_evek.Location = new System.Drawing.Point(119, 50);
+            this.comboBox_evek.Name = "comboBox_evek";
+            this.comboBox_evek.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_evek.TabIndex = 1;
+            // 
+            // btn_excel
+            // 
+            this.btn_excel.Location = new System.Drawing.Point(119, 92);
+            this.btn_excel.Name = "btn_excel";
+            this.btn_excel.Size = new System.Drawing.Size(121, 23);
+            this.btn_excel.TabIndex = 2;
+            this.btn_excel.Text = "excelbe mentés";
+            this.btn_excel.UseVisualStyleBackColor = true;
+            this.btn_excel.Click += new System.EventHandler(this.btn_excel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_excel);
+            this.Controls.Add(this.comboBox_evek);
+            this.Controls.Add(this.label_year);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_year;
+        private System.Windows.Forms.ComboBox comboBox_evek;
+        private System.Windows.Forms.Button btn_excel;
     }
 }
 
--- Olympics/Form1.cs
+++ Olympics/Form1.cs
@@ -6,29 +6,66 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Olympics
 {
     public partial class Form1 : Form
     {
         List<OlympicResult> results = new List<OlympicResult>();  //Hozz létre egy OlympicResult-okból álló listát a Form1 osztály szintjén results néven.
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
             InitializeComponent();
             betoltes("Summer_olympic_Medals.csv");
+            evvalasztas();
+            osztalyozas();
 
         }
 
+        private void osztalyozas()
+        {
+            foreach (OlympicResult item in results)
+            {
+                item.Position = helyezes(item);
+            }
+        }
+
+        int helyezes(OlympicResult re)
+        {
+            int szamlalo = 0;
+            var szurtlista = from x in results where x.Year == re.Year && x.Country != re.Country select x;
+            foreach (OlympicResult item in szurtlista)
+            {
+                if (item.Medals[0] > re.Medals[0]) szamlalo++;
+                else if ((item.Medals[0] == re.Medals[0]) && (item.Medals[1] > re.Medals[1])) szamlalo++;
+                else if ((item.Medals[0] == re.Medals[0]) && (item.Medals[1] == re.Medals[1]) && (item.Medals[2] > re.Medals[2])) szamlalo++;
+                //Egy ciklussal menj végig a szűrt listán. Növeld a számlálót, ha az alábbiak valamelyike teljesül
+            }
+            return szamlalo + 1; //Ezt követően a függvény térjen vissza a számlálónál eggyel nagyobb számmal (azaz a helyezéssel).
+        }
+
+        private void evvalasztas() //combobox feltölt
+        {
+            var years = (from x in results orderby x.Year select x.Year).Distinct();
+            //A függvényben írj LINQ lekérdezést, ami a results tartalmából lekérdezi az éveket, sorba rendezi azokat, és kiszűri a duplikátumokat
+            comboBox_evek.DataSource = years.ToList();  //combobox kell a .Tolist() 
+        }
+
+
         private void betoltes(string fajlnev)
         {
-            using (StreamReader sr= new StreamReader(fajlnev)) 
+            using (StreamReader sr = new StreamReader(fajlnev))
             {
                 sr.ReadLine(); //első sor beolvasása csv.ből fejléc - amivel nem csinálunk "semmit"
-                while (!sr.EndOfStream) 
+                while (!sr.EndOfStream)
                 {
                     string sor = sr.ReadLine(); //csv sora
                     string[] mezok = sor.Split(',');
@@ -42,10 +79,66 @@ namespace Olympics
                     mtomb[1] = int.Parse(mezok[6]); //ezüst
                     mtomb[2] = int.Parse(mezok[7]); //bronz
 
+                    peldany.Medals = mtomb;
                     results.Add(peldany);
 
                 }
-            } 
+            }
         }
+
+        private void btn_excel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                // Excel feltöltése
+                Excelfeltolt();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void Excelfeltolt()
+        {
+            var headers = new string[]
+            {
+               "Helyezés","Ország", "Arany", "Ezüst", "Bronz"
+            };
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells [1,i+1]= headers[i];
+            }
+
+            var filteredresult = from x in results where x.Year == (int)comboBox_evek.SelectedItem orderby x.Position select x;
+            //LINQ segítségével szűrd le a results tartalmát úgy, hogy csak a legördülő lista szerint kiválasztott éve értékei szerepeljenek benne, és az értékek a helyezés szerint legyenek sorba rendezve
+
+            int aktsor = 2; //2.sortól
+
+            foreach (var item in filteredresult)
+            {
+                xlSheet.Cells[aktsor, 1] = item.Position;
+                xlSheet.Cells[aktsor, 2] = item.Country;
+                for (int i = 0; i < 3; i++)
+                {
+                    xlSheet.Cells[aktsor, 3+i] = item.Medals[i];
+                }
+                aktsor++;
+
+            }
+        }
+
     }
 }
