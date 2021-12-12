1fc624c300cfac56818a86eab1f9cf7208fd513d
Áron Tóth <aron.toth2@stud.uni-corvinus.hu>
2021-12-12 15:10:19 +0100

    Labdagyár létrehozása

---
+++ Telapo_gyar/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telapo_gyar.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()  //függvénye CreateNew néven Ball visszatérési értékkel.A függvényben hozz létre egy Ball példányt és add vissza az értékét.
+        {
+            return new Ball();
+        }
+    }
+}
