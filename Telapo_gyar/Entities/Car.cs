2c7f3712eff18ea44fa3d70d0124468c3c1f11ad
Áron Tóth <aron.toth2@stud.uni-corvinus.hu>
2021-12-12 18:07:50 +0100

    Autó létrehozása

---
+++ Telapo_gyar/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telapo_gyar.Abstractions;
+
+namespace Telapo_gyar.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imagefajl = Image.FromFile("Images/car.png");
+            g.DrawImage(imagefajl,new Rectangle(0,0,Width,Height));
+        }
+    }
+}
