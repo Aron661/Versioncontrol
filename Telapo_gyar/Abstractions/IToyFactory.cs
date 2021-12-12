de3d1138b2782ccf051f07312a742630ae693aac
Áron Tóth <aron.toth2@stud.uni-corvinus.hu>
2021-12-12 20:07:02 +0100

    Gyár absztrakció kezelése

---
+++ Telapo_gyar/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telapo_gyar.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew(); // törzs nélküli metódus - imlementálás interfacenél
+    }
+}
