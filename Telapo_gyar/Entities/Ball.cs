6d2ca0d7b0aa0a452de0ba13209d89863fdb7568
Áron Tóth <aron.toth2@stud.uni-corvinus.hu>
2021-12-12 14:58:50 +0100

    Labda létrehozása

---
+++ Telapo_gyar/Entities/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Telapo_gyar.Entities
+{
+    public class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = Height = 50;
+            Paint += Ball_Paint; // eseménykezelőt a Paint eseményéhez.
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        private void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height); // kitöltött kék kört a Graphics osztály segítségével. 
+        }
+        public void MoveBall()
+        {
+            Left++; //v. Left += 1
+        }
+    }
+}
