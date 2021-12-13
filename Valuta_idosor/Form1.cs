7478ed7a27bd1a1f677f51257839e8b4436c93fe
Áron Tóth <aron.toth2@stud.uni-corvinus.hu>
2021-12-13 14:50:14 +0100

    XML feldolgozása

--- Valuta_idosor/Form1.cs
+++ Valuta_idosor/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using Valuta_idosor.Entities;
 using Valuta_idosor.MNBServiceReference;
 
@@ -20,11 +21,33 @@ namespace Valuta_idosor
         public Form1()
         {
             InitializeComponent();
-            GetExchangeRates();
+            LoadXml(GetExchangeRates());        //3.GetExchangeRates() ami string átad Loadxmlbe
             dataGridView_Rates.DataSource = Rates;
         }
 
-        private void GetExchangeRates()
+        private void LoadXml(string result) //4. (string ..)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(result); //5. = át kell adnod a korábbi result-ot ennek a függvénynek
+
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                RateData r = new RateData();
+                r.Date = DateTime.Parse(item.GetAttribute("date")); //item.GetAttribute("date");
+
+                var childElement = (XmlElement)item.ChildNodes[0];
+                r.Currency = childElement.GetAttribute("curr");
+
+                decimal unit = decimal.Parse(childElement.GetAttribute("unit")); //egész szám (1 és felkészülni hogy nem 0- osztás m)
+                r.Value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    r.Value = r.Value / unit;
+
+                Rates.Add(r);/////
+            }
+        }
+
+        private string GetExchangeRates() //1.string bemeneti értékre vált
         {
             var mnbService = new MNBArfolyamServiceSoapClient(); //MNBServiceReference.MNBArfolyamServiceSoapClient() a rövidítés miatt - MNBServiceReference usinggal (quick..)
             GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
@@ -33,7 +56,7 @@ namespace Valuta_idosor
             request.endDate = "2020-06-30";
 
             var response = mnbService.GetExchangeRates(request); //Hívd meg az mnbService GetExchangeRates nevű függvényét a request bemeneti paraméterrel, és a függvény visszatérési értékét tárold egy response nevű változóba
-            var result = response.GetExchangeRatesResult;
+            return response.GetExchangeRatesResult;  //2. var result helyett - return kimenet
             //File.WriteAllText("teszt.xml", result);  - kiiratás result xml - debug
 
         }
